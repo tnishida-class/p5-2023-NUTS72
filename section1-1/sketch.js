7658573bf3754186630102141e0842da6e7e15ce
NM <2240606h@stu.kobe-u.ac.jp>
2023-10-09 17:24:49 +0900

    Change section1-1
    櫻坂のピンクの枠線にする方法まで辿り着けませんでした。

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -5,10 +5,12 @@ function setup() {
 function draw() {
   background(0);
   strokeWeight(4);
-  fill(178, 100, 162);
+  fill(125,65,94);
   triangle(12,120,116,120,116,10);
-  fill(94, 185, 84);
+
+  fill(246,173,198);
   triangle(0,108,106,108,106,0);
+
   fill(255);
   textSize(32);
   textFont("serif");
